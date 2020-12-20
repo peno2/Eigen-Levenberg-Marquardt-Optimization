c1a008e23cb65c2dd255da7b687f4305938be7cc
peno2 <peno2@users.noreply.github.com>
2020-12-20 22:13:11 +0100

    Add a new simpler variant of minimizing function
    First version with 3 parameters.

---
+++ main2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+#include <unsupported/Eigen/NonLinearOptimization>
+#include <unsupported/Eigen/NumericalDiff>
+
+namespace {
+double warp2(const Eigen::VectorXd &x) { return  fabs(x(0) - 16) + fabs(x(1) - 8); }
+double warp3(const Eigen::VectorXd &x) { return  fabs(x(0) - 15) + fabs(x(1) - 7) + fabs(x(2) + 9); }
+}
+
+// Generic functor
+template<typename Scalar_, int NX = Eigen::Dynamic, int NY = Eigen::Dynamic>
+struct Functor {
+  using Scalar = Scalar_;
+  enum {
+	InputsAtCompileTime = NX,
+	ValuesAtCompileTime = NY
+  };
+  using  InputType = Eigen::Matrix<Scalar, InputsAtCompileTime, 1>;
+  using  ValueType = Eigen::Matrix<Scalar, ValuesAtCompileTime, 1>;
+  using  JacobianType = Eigen::Matrix<Scalar, ValuesAtCompileTime, InputsAtCompileTime>;
+
+  int m_inputs, m_values;
+
+  Functor() : m_inputs(InputsAtCompileTime), m_values(ValuesAtCompileTime) {}
+  Functor(int inputs, int values) : m_inputs(inputs), m_values(values) {}
+
+  int inputs() const { return m_inputs; }
+  int values() const { return m_values; }
+
+};
+
+struct my_functor : Functor<double> {
+  my_functor(): Functor<double>(3,3) {}
+  int operator()(const Eigen::VectorXd &x, Eigen::VectorXd &fvec) const {
+    fvec(0) = warp3(x); //fabs(x(0)+30) +  fabs(x(1)*x(1)-25);
+    fvec(1) = 0;
+
+    return 0;
+  }
+};
+
+
+int main(int argc, char *argv[]) {
+  Eigen::VectorXd x(3);
+  std::cout << "x:\n" << x << std::endl;
+
+  const my_functor functor;
+  Eigen::NumericalDiff<my_functor> numDiff(functor);
+  Eigen::LevenbergMarquardt<Eigen::NumericalDiff<my_functor>,double> lm(numDiff);
+  lm.parameters.maxfev = 2000;
+  lm.parameters.xtol = 1.0e-10;
+  std::cout << "Maxfev: " << lm.parameters.maxfev << std::endl;
+
+  const int ret = lm.minimize(x);
+  std::cout << "Iter : " << lm.iter << "\n"
+	    << "Ret: " << ret << "\n"
+	    << "x that minimizes the function:\n" << x << "\n";
+  return 0;
+}

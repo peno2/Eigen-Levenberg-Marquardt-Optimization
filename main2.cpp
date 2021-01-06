d5f16cb1fc351d623f54b21dee281629801264b6
peno2 <peno2@users.noreply.github.com>
2021-01-06 17:12:57 +0100

    Add a custom Double class
    Currently it build with some mods to Eigen. Probably these mods can be removed.

--- main2.cpp
+++ main2.cpp
@@ -5,9 +5,148 @@
 #include <unsupported/Eigen/NonLinearOptimization>
 #include <unsupported/Eigen/NumericalDiff>
 
+// From: https://stackoverflow.com/questions/59747377/custom-scalar-type-in-eigen
+class MyDouble {
+    public:
+        double value;
+        MyDouble() : value() {};
+        MyDouble(double val) : value(val) {};
+
+        template<typename T>
+            MyDouble& operator=(T rhs) {
+                value = static_cast<double>(rhs);
+                return *this;
+            }
+
+        template<typename T>
+            MyDouble& operator+=(T rhs) {
+                value = static_cast<double>(value + rhs.value);
+                return *this;
+            }
+
+        template<typename T>
+            MyDouble& operator-=(const T &rhs) {
+                value = static_cast<double>(value - rhs.value);
+                return *this;
+            }
+
+        template<typename T>
+            MyDouble& operator*=(T rhs) {
+                value = static_cast<double>(value * rhs.value);
+                return *this;
+            }
+
+        template<typename T>
+            MyDouble& operator/=(T rhs) {
+                value = static_cast<double>(value / rhs.value);
+                return *this;
+            }
+
+        MyDouble operator-() const {
+            return -value;
+        }
+
+        friend std::ostream& operator<<(std::ostream& out, const MyDouble& val) {
+            out << val.value << " m";
+            return out;
+        }
+
+        operator double() const {
+            return value;
+        }
+};
+
+#define OVERLOAD_OPERATOR(op,ret) ret operator op(const MyDouble &lhs, const MyDouble &rhs) { \
+        return lhs.value op rhs.value; \
+    }
+
+#define OVERLOAD_OPERATOR_D0(op,ret) ret operator op(const double &lhs, const MyDouble &rhs) { \
+        return lhs op rhs.value; \
+    }
+
+#define OVERLOAD_OPERATOR_D(op,ret) ret operator op(const MyDouble &lhs, const double &rhs) { \
+        return lhs.value op rhs; \
+    }
+
+#define OVERLOAD_OPERATOR_I0(op,ret) ret operator op(const int &lhs, const MyDouble &rhs) { \
+        return lhs op rhs.value; \
+    }
+
+#define OVERLOAD_OPERATOR_I1(op,ret) ret operator op(const MyDouble &lhs, const int &rhs) { \
+        return lhs.value op rhs; \
+    }
+
+OVERLOAD_OPERATOR(+, MyDouble)
+OVERLOAD_OPERATOR(-, MyDouble)
+OVERLOAD_OPERATOR(*, MyDouble)
+OVERLOAD_OPERATOR(/, MyDouble)
+
+OVERLOAD_OPERATOR(>, bool)
+OVERLOAD_OPERATOR(<, bool)
+OVERLOAD_OPERATOR(>=, bool)
+OVERLOAD_OPERATOR(<=, bool)
+OVERLOAD_OPERATOR(==, bool)
+OVERLOAD_OPERATOR(!=, bool)
+
+OVERLOAD_OPERATOR_D(+, MyDouble)
+OVERLOAD_OPERATOR_D(-, MyDouble)
+OVERLOAD_OPERATOR_D(*, MyDouble)
+OVERLOAD_OPERATOR_D(/, MyDouble)
+
+OVERLOAD_OPERATOR_D(>, bool)
+OVERLOAD_OPERATOR_D(<, bool)
+OVERLOAD_OPERATOR_D(>=, bool)
+OVERLOAD_OPERATOR_D(<=, bool)
+OVERLOAD_OPERATOR_D(==, bool)
+OVERLOAD_OPERATOR_D(!=, bool)
+
+OVERLOAD_OPERATOR_I0(*, MyDouble)
+OVERLOAD_OPERATOR_D0(-, MyDouble)
+OVERLOAD_OPERATOR_I1(!=, bool)
+
+MyDouble sqrt(MyDouble val) {
+    return std::sqrt(val.value);
+}
+MyDouble abs(MyDouble val) {
+    return std::abs(val.value);
+}
+MyDouble abs2(MyDouble val) {
+    return val * val;
+}
+bool isfinite(const MyDouble &) { return true; }
+
+namespace Eigen {
+    template<> struct NumTraits<MyDouble>
+        : NumTraits<double> // permits to get the epsilon, dummy_precision, lowest, highest functions
+        {
+            typedef MyDouble Real;
+            typedef MyDouble NonInteger;
+            typedef MyDouble Nested;
+            enum {
+                IsComplex = 0,
+                IsInteger = 0,
+                IsSigned = 1,
+                RequireInitialization = 0,
+                ReadCost = 1,
+                AddCost = 3,
+                MulCost = 3
+            };
+	  inline static MyDouble epsilon() { return NumTraits<double>::epsilon(); }
+        };
+
+    template<typename BinaryOp>
+    struct ScalarBinaryOpTraits<MyDouble,double,BinaryOp> { typedef MyDouble ReturnType;  };
+
+    template<typename BinaryOp>
+    struct ScalarBinaryOpTraits<double,MyDouble,BinaryOp> { typedef MyDouble ReturnType;  };
+}
+
+using MyType = MyDouble;
+using MyVec = Eigen::Matrix<MyType, Eigen::Dynamic, 1>;
+
 namespace {
-double warp2(const Eigen::VectorXd &x) { return  fabs(x(0) - 16) + fabs(x(1) - 8); }
-double warp3(const Eigen::VectorXd &x) { return  fabs(x(0) - 15) + fabs(x(1) - 7) + fabs(x(2) + 9); }
+  double warp2(const MyVec &x) { return  fabs(x(0) - MyType{16.}) + fabs(x(1) - MyType{8.}); }
+  double warp3(const MyVec &x) { return  fabs(x(0) - MyType{15}) + fabs(x(1) - MyType{7}) + fabs(x(2) + MyType{9}); }
 }
 
 constexpr int noOfParams = 2;
@@ -35,9 +174,9 @@ private:
   int m_inputs, m_values;
 };
 
-struct my_functor : Functor<double> {
-  my_functor(): Functor<double>(1, noOfParams) {}
-  int operator()(const Eigen::VectorXd &x, Eigen::VectorXd &fvec) const {
+struct my_functor : Functor<MyType> {
+  my_functor(): Functor<MyType>(1, noOfParams) {}
+  int operator()(const MyVec &x, MyVec &fvec) const {
     fvec(0) = warp2(x);
 
     return 0;
@@ -46,13 +185,13 @@ struct my_functor : Functor<double> {
 
 
 int main(int argc, char *argv[]) {
-  Eigen::VectorXd x(noOfParams);
+  MyVec x(noOfParams);
   std::cout << "Inital value of x:\n" << x << std::endl;
 
   const my_functor functor;
-  const double minEpsilon = 1;
+  const MyType minEpsilon = 1;
   Eigen::NumericalDiff<my_functor> numDiff(functor, minEpsilon);
-  Eigen::LevenbergMarquardt<Eigen::NumericalDiff<my_functor>,double> lm(numDiff);
+  Eigen::LevenbergMarquardt<Eigen::NumericalDiff<my_functor>, MyType> lm(numDiff);
   lm.parameters.maxfev = 2000;
   lm.parameters.xtol = 1.0e-10;
   std::cout << "Maxfev: " << lm.parameters.maxfev << std::endl;
